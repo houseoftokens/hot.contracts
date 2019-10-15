3dacdc93febb2691825325a44721bdbfcb9ef062
House Token <housetoken@hotmail.com>
2019-10-15 11:13:42 +0000

    token contrat support bonus

--- contracts/eosio.token/include/eosio.token/eosio.token.hpp
+++ contracts/eosio.token/include/eosio.token/eosio.token.hpp
@@ -2,6 +2,7 @@
 
 #include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/singleton.hpp>
 
 #include <string>
 
@@ -39,6 +40,18 @@ namespace eosio {
          [[eosio::action]]
          void close( name owner, const symbol& symbol );
 
+         [[eosio::action]]
+         void bonusfreeze( asset bonus, asset minimum, name collector );
+
+         [[eosio::action]]
+         void bonusclear();
+
+         [[eosio::action]]
+         void bonus( name to );
+
+         [[eosio::action]]
+         void bonusclose( bool force );
+
          static asset get_supply( name token_contract_account, symbol_code sym_code )
          {
             stats statstable( token_contract_account, sym_code.raw() );
@@ -59,11 +72,42 @@ namespace eosio {
          using transfer_action = eosio::action_wrapper<"transfer"_n, &token::transfer>;
          using open_action = eosio::action_wrapper<"open"_n, &token::open>;
          using close_action = eosio::action_wrapper<"close"_n, &token::close>;
+         using bonusfreeze_action = eosio::action_wrapper<"bonusfreeze"_n, &token::bonusfreeze>;
+         using bonusclear_action = eosio::action_wrapper<"bonusclear"_n, &token::bonusclear>;
+         using bonus_action = eosio::action_wrapper<"bonus"_n, &token::bonus>;
+         using bonusclose_action = eosio::action_wrapper<"bonusclose"_n, &token::bonusclose>;
+
       private:
+
+         // account bonus based on meta data of balance
+         struct [[eosio::table]] account_bonus_meta {
+            name      owner;        // bonus meta owner
+            uint64_t  round;        // current bonus round
+            int64_t   balance;      // bonus base balnace of *this* round
+            asset     bonus;        // uncleared bonus shares
+
+            uint64_t primary_key() const { return owner.value; }
+            uint64_t bonus_round_key() const { return round; }
+            uint64_t bonus_amount_key() const { return bonus.amount; }
+         };
+      
+         // bonus round infomation
+         struct [[eosio::table]] bonus_round {
+            uint64_t  id;
+            uint64_t  round;        // round num
+            bool      clearing;     // whether we are clearing this round
+            int64_t   clearbase;    // core asset supply on the time of round freeze
+            asset     bonus;        // actual total bonus of this round
+            asset     minmum_bonus; // if one got bonus less than this, then it get nothing
+            asset     balance;      // dynamic balance during clearing
+            name      collector;    // account who got balance after clear
+
+            uint64_t primary_key() const { return id; }
+         };
+
          struct [[eosio::table]] account {
             asset    balance;
-
-            uint64_t primary_key()const { return balance.symbol.code().raw(); }
+            uint64_t primary_key() const { return balance.symbol.code().raw(); }
          };
 
          struct [[eosio::table]] currency_stats {
@@ -71,14 +115,22 @@ namespace eosio {
             asset    max_supply;
             name     issuer;
 
-            uint64_t primary_key()const { return supply.symbol.code().raw(); }
+            uint64_t primary_key() const { return supply.symbol.code().raw(); }
          };
 
          typedef eosio::multi_index< "accounts"_n, account > accounts;
          typedef eosio::multi_index< "stat"_n, currency_stats > stats;
 
+         typedef eosio::multi_index< "abms"_n, account_bonus_meta,
+            indexed_by<"byround"_n, const_mem_fun<account_bonus_meta, uint64_t, &account_bonus_meta::bonus_round_key> >,
+            indexed_by<"bybonus"_n, const_mem_fun<account_bonus_meta, uint64_t, &account_bonus_meta::bonus_amount_key> > 
+         > abms;
+         typedef eosio::multi_index< "brnd"_n, bonus_round > brnd;
+
          void sub_balance( name owner, asset value );
          void add_balance( name owner, asset value, name ram_payer );
+         void on_balance_change(name owner, asset balance, name ram_payer);
+         asset calc_bonus(uint64_t balance) const;
    };
 
 } /// namespace eosio
--- contracts/eosio.token/src/eosio.token.cpp
+++ contracts/eosio.token/src/eosio.token.cpp
@@ -1,5 +1,9 @@
 #include <eosio.token/eosio.token.hpp>
 
+#define HOT_CORE_SYMBOL (symbol("HOT", 6))
+#define HOT_BONUS_SCOPE 0
+#define HOT_BONUS_ACT_PER_ROUND 8
+
 namespace eosio {
 
 void token::create( name   issuer,
@@ -106,29 +110,95 @@ void token::transfer( name    from,
 }
 
 void token::sub_balance( name owner, asset value ) {
-   accounts from_acnts( _self, owner.value );
-
-   const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
-   check( from.balance.amount >= value.amount, "overdrawn balance" );
-
-   from_acnts.modify( from, owner, [&]( auto& a ) {
-         a.balance -= value;
-      });
+    accounts from_acnts( _self, owner.value );
+    
+    const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
+    check( from.balance.amount >= value.amount, "overdrawn balance" );
+    asset balance;
+    from_acnts.modify( from, owner, [&]( auto& a ) {
+       a.balance -= value;
+       balance = a.balance;
+    });
+    on_balance_change(owner, balance, owner);
 }
 
 void token::add_balance( name owner, asset value, name ram_payer )
 {
    accounts to_acnts( _self, owner.value );
    auto to = to_acnts.find( value.symbol.code().raw() );
+   asset balance;
    if( to == to_acnts.end() ) {
       to_acnts.emplace( ram_payer, [&]( auto& a ){
         a.balance = value;
+        balance = a.balance;
       });
    } else {
       to_acnts.modify( to, same_payer, [&]( auto& a ) {
         a.balance += value;
-      });
+        balance = a.balance;
+      });      
    }
+   on_balance_change(owner, balance, ram_payer);
+}
+
+void token::on_balance_change(name owner, asset balance, name ram_payer)
+{
+   // only update on core asset balance changing
+   if ( balance.symbol != HOT_CORE_SYMBOL ) {
+      return;
+   }
+   // find newest round number
+   brnd bonus_rounds( _self, HOT_BONUS_SCOPE );
+   uint64_t round_num = 0;
+   auto it_latest = bonus_rounds.begin();
+   if (it_latest != bonus_rounds.end()) {
+      round_num = it_latest->round;
+   }
+
+   // update bonus meta
+   abms to_abms( _self, HOT_BONUS_SCOPE );
+   auto it_to = to_abms.find( owner.value );
+   if ( it_to == to_abms.end() ) {
+      // if no abms record, create a new one
+      to_abms.emplace( ram_payer, [&]( auto &m ) {
+         m.owner = owner;
+         m.round = round_num;
+         m.balance = balance.amount;
+         m.bonus = asset();
+      });
+   } else {
+      // compare current round number to abms
+      if ( it_to->round + 1 == round_num ) {
+         // new round is started since last update
+         to_abms.modify( it_to, same_payer, [&]( auto &m ) {   
+            m.bonus = calc_bonus( m.balance );
+            m.round = round_num;
+            m.balance = balance.amount;
+         });
+      } else if ( it_to->round == round_num ) {
+         // update abms record
+         to_abms.modify( it_to, same_payer, [&]( auto &m ) {
+            m.balance = balance.amount;
+         });
+      } else {
+         // this should not happen
+         check( false, "abms round number should <= current round number" );
+      }
+   }
+}
+
+// caculate bonus of last round
+asset token::calc_bonus(uint64_t balance) const {
+   brnd bonus_rounds( _self, HOT_BONUS_SCOPE );
+   auto it = bonus_rounds.begin();
+   check( it != bonus_rounds.end(), "bonus round not found" );
+   check( it->clearing, "calc_bonus should be called during clearing" );
+   uint128_t bonus_val = uint128_t(it->bonus.amount) * uint128_t(balance);
+   bonus_val /= uint128_t(it->clearbase);
+   // asset bonus = it->bonus * balance / it->clearbase;
+   asset bonus = it->bonus;
+   bonus.amount = uint64_t(bonus_val);
+   return bonus;
 }
 
 void token::open( name owner, const symbol& symbol, name ram_payer )
@@ -148,6 +218,7 @@ void token::open( name owner, const symbol& symbol, name ram_payer )
       acnts.emplace( ram_payer, [&]( auto& a ){
         a.balance = asset{0, symbol};
       });
+      on_balance_change(owner, asset{0, symbol}, ram_payer);
    }
 }
 
@@ -161,6 +232,236 @@ void token::close( name owner, const symbol& symbol )
    acnts.erase( it );
 }
 
+void token::bonusfreeze( asset bonus, asset minimum, name collector )
+{
+   check( bonus.is_valid(), "invalid bonus quantity" );
+   check( minimum.is_valid(), "invalid minimum quantity" );
+   check( bonus.symbol == minimum.symbol, "bonus and minimum should be the same token" );
+   check( bonus.amount > 0, "bonus amount should greator than 0" );
+   check( bonus.amount > minimum.amount, "bonus amount should greator than minimum amount" );
+   check( is_account( collector ), "collector account does not exist");
+
+   // check existance of bonus token
+   stats stat_bonus( _self, bonus.symbol.code().raw() );
+   auto it_stat_bonus = stat_bonus.find( bonus.symbol.code().raw() );
+   check( it_stat_bonus != stat_bonus.end(), "token with bonus symbol does not exist, cannot freeze bonus" );
+   const auto& st = *it_stat_bonus;
+   require_auth( st.issuer );
+
+   // get current CORE ASSET supply
+   stats stat_core( _self, HOT_CORE_SYMBOL.code().raw() );
+   auto it_stat_core = stat_core.find( HOT_CORE_SYMBOL.code().raw() );
+   check( it_stat_core != stat_core.end(), "core asset token does not exist, cannot freeze bonus" );
+   uint64_t supply = it_stat_core->supply.amount;
+
+   // update round info
+   brnd bonus_rounds( _self, HOT_BONUS_SCOPE );
+   auto it_br = bonus_rounds.begin();
+   if ( it_br == bonus_rounds.end() ) {
+      // this is the first time freeze
+      bonus_rounds.emplace(st.issuer, [&]( auto &br ) {
+         br.id = 1;
+         br.round = 1;
+         br.clearing = true;
+         br.clearbase = supply;
+         br.bonus = bonus;
+         br.minmum_bonus = minimum;
+         br.balance = bonus;
+         br.collector = collector;
+      });
+   } else {
+      check(!it_br->clearing, "in process of clearing, cannot freeze bonus");
+      bonus_rounds.modify( *it_br, same_payer, [&]( auto &br ) {
+         br.round += 1;
+         br.clearing = true;
+         br.clearbase = supply;
+         br.bonus = bonus;
+         br.minmum_bonus = minimum;
+         br.balance = bonus;
+         br.collector = collector;
+      });
+   }
+}
+
+void token::bonusclear()
+{
+   brnd bonus_rounds( _self, HOT_BONUS_SCOPE );
+   auto it_br = bonus_rounds.begin();
+   check( it_br != bonus_rounds.end(), "bonus round not found" );
+   check( it_br->clearing, "bonus round has not been frozen yet" );
+
+   stats statstable( _self, it_br->bonus.symbol.code().raw() );
+   auto existing = statstable.find( it_br->bonus.symbol.code().raw() );
+   check( existing != statstable.end(), "token with bonus symbol does not exist, cannot do bonus clear" );
+   const auto& st = *existing;
+   require_auth( st.issuer );
+
+   std::vector<name> bonus_accs;
+   bool done_clear = false;
+   abms to_abms( _self, HOT_BONUS_SCOPE );
+
+   // check account whose balance does not update during freeze
+   auto idx_rnd = to_abms.get_index<"byround"_n>();
+   for ( auto it = idx_rnd.begin(); it != idx_rnd.end() && it->round < it_br->round; ++it ) {
+      // maximum action per transaction
+      if ( bonus_accs.size() >= HOT_BONUS_ACT_PER_ROUND ) {
+         break;
+      }
+      bonus_accs.push_back(it->owner);
+   }
+
+   if ( bonus_accs.size() < HOT_BONUS_ACT_PER_ROUND ) {
+      // check account whose balance has updated during freeze
+      auto index = to_abms.get_index<"bybonus"_n>();
+      for (auto it = index.rbegin(); bonus_accs.size() < HOT_BONUS_ACT_PER_ROUND; ++it) {
+         if (it == index.rend() || it->bonus.amount == 0) {
+            // done clear
+            done_clear = true;
+            break;
+         }
+         bonus_accs.push_back(it->owner);
+      }
+   }
+
+   // send acctions for bonus
+   for (auto acc = bonus_accs.begin(); acc != bonus_accs.end(); ++acc) {
+      SEND_INLINE_ACTION( *this, bonus, { {st.issuer, "active"_n} }, { *acc } );
+   }
+
+   // we are done clear
+   if (done_clear) {
+      auto it_br = bonus_rounds.begin();
+      check( it_br != bonus_rounds.end(), "bonus round not found" );
+      if (it_br->balance.amount > 0) {
+         // send all left bonus to issuer
+         SEND_INLINE_ACTION( *this, bonusclose, { {st.issuer, "active"_n} }, { false } );
+      }
+      // // mark this round as finished
+      // bonus_rounds.modify( *it_br, same_payer, [&]( auto& br ) {
+      //    br.clearing = false;
+      //    br.clearbase = 0;
+      //    br.bonus = asset();
+      //    br.minmum_bonus = asset();
+      //    br.balance = asset();
+      //    br.collector = name();
+      // });
+   }
+}
+
+void token::bonus(name to )
+{
+   brnd bonus_rounds( _self, HOT_BONUS_SCOPE );
+   auto it_br = bonus_rounds.begin();
+   check( it_br != bonus_rounds.end(), "bonus round not found, could not bonus" );
+   check( it_br->clearing, "bonus round not frozen yet, could not bonus" );
+
+   stats statstable( _self, it_br->bonus.symbol.code().raw() );
+   auto existing = statstable.find( it_br->bonus.symbol.code().raw() );
+   check( existing != statstable.end(), "token with bonus symbol does not exist, create token before bonus" );
+   const auto& st = *existing;
+   require_auth( st.issuer );
+
+   abms to_abms( _self, HOT_BONUS_SCOPE );
+   auto it_abms = to_abms.find( to.value );
+   check( it_abms != to_abms.end(), "abms not found, could not bonus" );
+
+   if ( it_abms->round + 1 == it_br->round ) {
+      // not balance update happen after freeze
+      to_abms.modify( it_abms, same_payer, [&]( auto &m ) {   
+         m.bonus = calc_bonus( m.balance );
+         m.round = it_br->round;
+      });
+      // fetch abms after modfication
+      it_abms = to_abms.find( to.value );
+      check( it_abms != to_abms.end(), "abms not found, could not bonus" );
+   } else if (it_abms->round != it_br->round) {
+      check(false, "round number not match, this should not happen");
+   }
+
+   check( it_br->bonus.symbol == it_abms->bonus.symbol, "bonus symbol should be the same" );
+   // check( it_abms->bonus.amount > 0, "abms bonus should greater than 0" );
+
+   auto real_bonus = it_abms->bonus;
+   to_abms.modify( it_abms, same_payer, [&]( auto& a ) {
+      a.bonus = asset();
+   });
+
+   // if bonus is less than minimum bonus amount, do not bonus
+   if (real_bonus.amount < it_br->minmum_bonus.amount) {
+      return;
+   }
+   // run actual bonus
+   check( real_bonus.amount <= st.max_supply.amount - st.supply.amount, "bonus quantity exceeds available supply");
+
+   // update round balance
+   bonus_rounds.modify( *it_br, same_payer, [&]( auto &br ) {
+      br.balance -= real_bonus;
+   });
+
+   // following logic likes issue
+   statstable.modify( st, same_payer, [&]( auto& s ) {
+      s.supply += real_bonus;
+   });
+
+   add_balance( st.issuer, real_bonus, st.issuer );
+
+   if( to != st.issuer ) {
+      SEND_INLINE_ACTION( *this, transfer, { {st.issuer, "active"_n} },
+            { st.issuer, to, real_bonus, "bonus" }
+      );
+   }
+}
+
+void token::bonusclose( bool force ) {
+   brnd bonus_rounds( _self, HOT_BONUS_SCOPE );
+   auto it_br = bonus_rounds.begin();
+   check( it_br != bonus_rounds.end(), "bonus round not found" );
+   check( it_br->clearing, "bonus round has not been frozen yet" );
+
+   stats statstable( _self, it_br->bonus.symbol.code().raw() );
+   auto existing = statstable.find( it_br->bonus.symbol.code().raw() );
+   check( existing != statstable.end(), "token with bonus symbol does not exist, create token before bonusclose" );
+   const auto& st = *existing;
+
+   if ( !force ) {
+      require_auth( st.issuer );
+   } else {
+      // super user auth required
+      require_auth( _self );
+   }
+
+   abms to_abms( _self, HOT_BONUS_SCOPE );
+   auto idx_rnd = to_abms.get_index<"byround"_n>();
+   auto it = idx_rnd.find(it_br->round - 1);
+   check( it == idx_rnd.end(), "when bonus close, there should be no abms round smaller than current round number" );
+   
+   auto index = to_abms.get_index<"bybonus"_n>();
+   auto it_bn = index.rbegin();
+   if ( it_bn != index.rend() ) {
+      check( it_bn->bonus.amount <= 0, "when bonus close, there should be no abms with bonus greater than 0" );
+   }
+
+   auto real_bonus = it_br->balance;
+   auto collector = it_br->collector;
+   bonus_rounds.modify( *it_br, same_payer, [&]( auto& br ) {
+      br.clearing = false;
+      br.clearbase = 0;
+      br.bonus = asset();
+      br.minmum_bonus = asset();
+      br.balance = asset();
+      br.collector = name();
+   });
+
+   if ( real_bonus.amount > 0 ) {
+      add_balance( st.issuer, real_bonus, st.issuer );
+      if( collector != st.issuer ) {
+         SEND_INLINE_ACTION( *this, transfer, { {st.issuer, "active"_n} },
+               { st.issuer, collector, real_bonus, "bonus close" }
+         );
+      }      
+   }
+}
+
 } /// namespace eosio
 
-EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(close)(retire) )
+EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(close)(retire)(bonusfreeze)(bonusclear)(bonus)(bonusclose) )
